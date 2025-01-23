1d35f50adee639b13459d3a3f5da69b7c5406ffd
roy. <roywashington1547@gmail.com>
2025-01-22 19:00:21 -0600

    simple site example

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import home from './index.html';
+
+Bun.serve({
+  port: 3000,
+  static: {
+    '/': home,
+  },
+  async fetch(req) {
+    return new Response('Hello World');
+  },
+});
+
+console.log('Server is running on port 3000');
